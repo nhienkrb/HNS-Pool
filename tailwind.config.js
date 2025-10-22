61447e5bf00d80635344e54ac2ae04799ade2d43
nhiendev <nhiennkrb@gmail.com>
2025-10-22 15:37:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [], // hoặc xóa hoàn toàn dòng content
+  theme: {
+    extend: {
+      fontFamily: {
+        "great-vibes": ['"Great Vibes"', "cursive"], // class: font-great-vibes
+      },
+    },
+  },
+  plugins: [],
+};
