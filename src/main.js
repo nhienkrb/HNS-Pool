10014a6ce692333241053daeecd397d31dff5752
nhiendev <nhiennkrb@gmail.com>
2025-11-28 13:47:50 +0700

    fix: section gia tri co loi

--- src/main.js
+++ src/main.js
@@ -39,3 +39,44 @@ new Swiper(".myTimelineSwiper", {
     1280: { slidesPerView: 5 },
   }
 });
+
+
+const featureContent = {
+  1: {
+    title: "Tiêu đề cho icon 1",
+    desc: "Nội dung mô tả tương ứng icon 1."
+  },
+  2: {
+    title: "Tiêu đề cho icon 2",
+    desc: "Nội dung mô tả tương ứng icon 2."
+  },
+  3: {
+    title: "Tiêu đề cho icon 3",
+    desc: "Cam kết sản phẩm an toàn, ổn định, đáp ứng các tiêu chuẩn kiểm định nghiêm ngặt và không ngừng cải tiến để phù hợp với thị hiếu và nhu cầu của người tiêu dùng Việt."
+  },
+   4: {
+    title: "Tiêu đề cho icon 4",
+    desc: "Cam kết sản phẩm an toàn, ổn định, đáp ứng các tiêu chuẩn kiểm định nghiêm ngặt và không ngừng cải tiến để phù hợp với thị hiếu và nhu cầu của người tiêu dùng Việt."
+  },
+  5: {
+    title: "Sản phẩm chất lượng, đáp ứng nhu cầu khách hàng",
+    desc: "Cam kết sản phẩm an toàn, ổn định, đáp ứng các tiêu chuẩn kiểm định nghiêm ngặt và không ngừng cải tiến để phù hợp với thị hiếu và nhu cầu của người tiêu dùng Việt."
+  }
+};
+
+
+$(document).ready(function () {
+
+    $(".feature-icon").on("click", function () {
+
+        $(".feature-icon").removeClass("active bg-xanh text-white").addClass("bg-white");
+
+        $(this).addClass("active bg-xanh text-white").removeClass("bg-white");
+
+        const id = $(this).data("id");
+
+        $(".content-title").text(featureContent[id].title);
+        $(".content-desc").text(featureContent[id].desc);
+    });
+
+});
